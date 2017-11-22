e0442402f59a47e4ee8c336d9e6a0bb8e3026ef9
tyf <448371352@qq.com>
2017-11-22 23:36:29 +0800

    对service进行了相应的修改，秀出mdltree与resource有重叠，后期要抽离出来

--- src/services/mdltree.js
+++ src/services/mdltree.js
@@ -1,12 +1,21 @@
 import { request, config } from 'utils'
 
 const { api } = config
-const { mdl } = api
+const { mdltree,resources,resource } = api
+
 
 export async function query (params) {
   return request({
-    url: mdl,
+    url: resources,
     method: 'get',
+    data: params,
+  })
+}
+
+export async function update (params) {
+  return request({
+    url: resource,
+    method: 'patch',
     data: params,
   })
 }
